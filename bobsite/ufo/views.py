e62760d6853a29e90a7c0ac94d65ca72ba8d41c4
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-03 09:59:03 -0400

    finished parsing

--- bobsite/ufo/views.py
+++ bobsite/ufo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 import bobsite.settings as set
 from .forms import UserForm
+import pandas as pd
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,6 @@ def index(request):
      dict = {"form":UserForm}
      return( render(request,'index.html',context=dict))
 def getdata(request):
-    print("in getdata")
     dict1 = {"form":UserForm}
     state = request.POST['your_state']
     year  = request.POST['your_year']
