ac79522f6a895e150a48f93108e271729a3e424a
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-05 14:28:15 -0400

    first try at graphics

--- bobsite/ufo/views.py
+++ bobsite/ufo/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, HttpResponseNotFound
 import bobsite.settings as set
 from .forms import UserForm
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -24,7 +28,9 @@ def getdata(request):
       df['Year'] = df['Time'].apply(fetch_year)
       df['Day_of_week'] = df['Time'].apply(fetch_date)
       l_dict = {"listo":df.to_html()}
-      return render(request,"index.html",{"listo":df.to_html})
+      grouped = gen_bar_chart(df)
+      print(grouped)
+      return render(request,"index.html",{"listo":df.to_html,"charto":grouped})
     else:
        return (render(request,"index.html",context=dictstatus))  
 def fetch_year(x):
@@ -33,4 +39,15 @@ def fetch_date(x):
     day_dict={0:'Sunday',1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thurdday',5:'Friday',6:'Saturday'}
     return(day_dict[pd.to_datetime(x).weekday()])
 def gen_bar_chart(frame):
-    return('none')    
+    t = frame['Shape Reported'].value_counts()
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+    objects = t.index
+    y_pos = np.arange(len(objects))
+    performance = t.values
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    response=HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    print("about to return from gen_bar_chart ")
+    return response   
