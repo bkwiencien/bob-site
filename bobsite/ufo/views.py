6e29e1889ecf241f788dee8f388b1b12270000cc
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-08 15:09:17 -0400

    bar chart working decode utf-8

--- bobsite/ufo/views.py
+++ bobsite/ufo/views.py
@@ -4,7 +4,6 @@ import bobsite.settings as set
 from .forms import UserForm
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy as np
 import io
@@ -14,7 +13,6 @@ import base64
 
 # Create your views here.
 def index(request):
-     print("in views TEMPLATE_DIRS = {}".format(set.TEMPLATE_DIRS))
      dict = {"form":UserForm}
      return( render(request,'index.html',context=dict))
 def fetch_year(x):
@@ -23,7 +21,7 @@ def fetch_date(x):
     day_dict={0:'Sunday',1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thurdday',5:'Friday',6:'Saturday'}
     return(day_dict[pd.to_datetime(x).weekday()])
 def genbarchart(request):
-    #pdb.set_trace()
+    dictstatus = {"status":"no data for that timeframe"}
     state = request.POST['your_state']
     year  = request.POST['your_year']
     df = pd.read_csv('http://bit.ly/uforeports')
@@ -32,7 +30,6 @@ def genbarchart(request):
       df['Year'] = df['Time'].apply(fetch_year)
       df = df[df.Year==year]
       df['Day_of_week'] = df['Time'].apply(fetch_date)
-    print("in genbarchart !!!!!!")
     t = df['Shape Reported'].value_counts()
     fig = plt.figure()
     plt.rcParams["figure.figsize"] = [20,20]
@@ -41,12 +38,11 @@ def genbarchart(request):
     y_pos = np.arange(len(objects))
     plt.xticks(y_pos, objects)
     plt.bar(y_pos, performance, align='center', alpha=0.5)
-    print("about to render the graphics!!!!!!!! ")
     f = io.BytesIO()
     fig.savefig(f,format='png')
     f.seek(0)
     image=base64.b64encode(f.getvalue())
-    #return render(request,"index.html",{'charto':image})
+    image=str(image,'utf-8')
     if (year != 'ALL Years'):
       df['Year'] = df['Time'].apply(fetch_year)
       df = df[df.Year==year]
@@ -56,7 +52,6 @@ def genbarchart(request):
       df['Day_of_week'] = df['Time'].apply(fetch_date)
       l_dict = {"listo":df.to_html()}
       #pdb.set_trace()
-      print("about to call return render !!!")
       return render(request,"index.html",{"listo":df.to_html,'charto':image})
     else:
        return (render(request,"index.html",context=dictstatus)) 
