d32f06b8c215cd63dfb0c8cf7ff103450daa14b6
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-13 18:10:36 -0400

    textarea

--- bobsite/comments/views.py
+++ bobsite/comments/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 import bobsite.settings as set
-
+from .forms import CommentForm
 # Create your views here.
 
 def comments(request):
-     return( render(request,set.TEMPLATE_DIRS[1]+'/index.html'))
+     return( render(request,set.TEMPLATE_DIRS[1]+'/index.html',{"form":CommentForm}))
