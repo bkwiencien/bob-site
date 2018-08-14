65a470573774765f1086485452520908cb8047eb
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-14 17:12:00 -0400

    myql

--- bobsite/bobsite/settings.py
+++ bobsite/bobsite/settings.py
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'bobsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'comments',
         'USER': 'bob',
         'PASSWORD': 'bob',
