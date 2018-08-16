cb2addd9067a6162ba6b6f060e31a346d3d0117f
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-16 13:34:23 -0400

    addinf javascript

--- bobsite/comments/forms.py
+++ bobsite/comments/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CommentForm(forms.Form):
-    comment = forms.CharField(widget=forms.Textarea)
-    your_name = forms.CharField(label='Your name', max_length=100)  
+    comment = forms.CharField(label="comment", widget=forms.Textarea)
+    your_name = forms.CharField(label='email address', max_length=100)  
