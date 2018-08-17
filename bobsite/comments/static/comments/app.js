41f6ba7bda2995c860526db69c980970ef4e63f7
Bob Kwiencien <bkwiencien@gmail.com>
2018-08-17 10:10:08 -0400

    ajax call working

--- bobsite/comments/static/comments/app.js
+++ bobsite/comments/static/comments/app.js
@@ -1,9 +1,29 @@
 function checkEmail() {
 	var commento = document.getElementById("id_comment").value;
 	var emailo =  document.getElementById("id_your_name").value;
-	console.log('commento = ' + commento)
+	console.log('emailo = ' + emailo)
 	if (commento.length == 0) {
 		alert("comment cannot be blank")
 		return;
 	}
+    var res = validateEmail(emailo)
+    if (!res) {
+    	alert('email invalid')
+    	return;
+    }
+    $.post("/comments/getcomment/",
+    {
+        comment: commento,
+        your_name:  emailo,
+    },
+    function(data, status){
+    	console.log(data)
+        //window.location.href = data;
+        document.write(data)
+    });
+
+}
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
--- bobsite/comments/views.py
+++ bobsite/comments/views.py
@@ -3,13 +3,16 @@ from django.http import HttpResponse, HttpResponseNotFound
 import bobsite.settings as set
 from .forms import CommentForm
 from .models import Comments
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def comments(request):
      return( render(request,set.TEMPLATE_DIRS[1]+'/index.html',{"form":CommentForm}))
+@csrf_exempt     
 def getcomment(request):
      commento = request.POST.get('comment') 
      nameo    = request.POST.get('your_name')
+     print('in the view !!!!!!!!!!!!!!!!!!!!!! ' + commento + " " + nameo)
      p = Comments(comment=commento,name=nameo)
      p.save()
      return((render(request,set.TEMPLATE_DIRS[1]+'/thanks.html')))
